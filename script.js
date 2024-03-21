4147682137bb6fa7b83997e6b8d964f7cda8c47f
LuisE <luise.padillasantillan@gmail.com>
2024-03-21 12:27:12 -0600

    Kept doing js
    Made the computer only able to make it's move when the player makes theirs'

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let restartButton = document.getElementById("reset-button");
 let playButton = document.getElementById("play-button");
 let buttons = document.getElementsByName("user-choice");
 let sub = document.getElementById("sub-button");
-sub.addEventListener("click", check);
-
-function check()
+sub.addEventListener("click", () => {
+    checkWinner(checkUserchoice(), computerChoice())
+});
+function checkWinner(userMove, computerMove){
+    console.log(userMove, computerMove)
+}
+function checkUserchoice()
 {
     for(let button of buttons)
     {
@@ -15,27 +19,31 @@ function check()
             choiceImage.src = `images/${button.value}.png`;
 
             userChoice.append(choiceImage);
-
+            return button.value
         }   
     }
 }
-function coumputerChoice()
+function computerChoice()
 {
     choice = Math.floor(Math.random() * 3) + 1;
-    console.log(choice);
     choiceImage2 = document.createElement("img");
+    compMove = null;
     switch(choice)
     {
         case 1:
             choiceImage2.src = "images/rock.png";
+            compMove = "rock";
             break;
         case 2:
             choiceImage2.src = "images/paper.png";
+            compMove = "paper";
             break;
         case 3:
             choiceImage2.src = "images/scissors.png";
+            compMove = "scissors";
             break;
     }
     compChoice.append(choiceImage2);
+    return compMove;
 }
 
