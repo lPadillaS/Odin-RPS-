7fbe9545f00561b7d4698e008c2e3454a40cbbe3
LuisE <136264805+lPadillaS@users.noreply.github.com>
2024-03-22 18:51:36 -0600

    Kept doing js, deleted play button, css tweaks
    Added logic to find the winner of every round, also
    now the last choices get deleted from display divs.

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let compChoice = document.getElementById("computer-Choice");
 let userChoice = document.getElementById("player-Choice");
 let restartButton = document.getElementById("reset-button");
-let playButton = document.getElementById("play-button");
 let buttons = document.getElementsByName("user-choice");
 let sub = document.getElementById("sub-button");
 sub.addEventListener("click", () => {
     checkWinner(checkUserchoice(), computerChoice())
 });
 function checkWinner(userMove, computerMove){
-    console.log(userMove, computerMove)
+    if (userMove == computerMove){
+        console.log("it's a tie");
+    }
+    else if(userMove == "rock" && computerMove == "paper"){
+        console.log("computer wins");
+    }
+    else if (userMove == "paper" && computerMove == "rock"){
+        console.log("You win");
+    }
+    else if(userMove == "rock" && computerMove == "scissors"){
+        console.log("You win");
+    }
+    else if(userMove == "scissors" && computerMove == "rock"){
+        console.log("computer wins");
+    }
+    else if(userMove == "paper" && computerMove == "scissors"){
+        console.log("computer wins");
+    }
+    else if(userMove == "scissors" && computerMove == "paper"){
+        console.log("you win");
+    }
 }
 function checkUserchoice()
 {
@@ -16,10 +35,14 @@ function checkUserchoice()
     {
         if (button.checked){
             choiceImage = document.createElement("img");
+            choiceImage.id = "user-image";
             choiceImage.src = `images/${button.value}.png`;
-
+            let rem = document.getElementById("user-image");
+            if (rem){
+                rem.remove();
+            }
             userChoice.append(choiceImage);
-            return button.value
+            return button.value;
         }   
     }
 }
@@ -27,6 +50,7 @@ function computerChoice()
 {
     choice = Math.floor(Math.random() * 3) + 1;
     choiceImage2 = document.createElement("img");
+    choiceImage2.id = "computer-image";
     compMove = null;
     switch(choice)
     {
@@ -42,6 +66,10 @@ function computerChoice()
             choiceImage2.src = "images/scissors.png";
             compMove = "scissors";
             break;
+    }
+    let rem = document.getElementById("computer-image");
+    if (rem){
+        rem.remove();
     }
     compChoice.append(choiceImage2);
     return compMove;
